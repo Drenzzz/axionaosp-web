69f69f9f9ef5dbb0b71638901431e9d8cf525e03
Drenzzz <realdrenzzz@gmail.com>
2025-10-04 18:08:40 +0700

    chore(ops): configure scripts for dev, build, and production start

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,25 @@
 import { Elysia } from 'elysia';
+import { staticPlugin } from '@elysiajs/static';
 
-const app = new Elysia()
-  .get('/', () => 'Hello World!')
-  .listen(3001);
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = new Elysia();
+
+  if (isProduction) {
+    app
+      .use(
+        staticPlugin({
+          assets: '../dist',
+          prefix: '',
+        })
+      )
+      .get('/*', ({ set }) => {
+        set.headers['Content-Type'] = 'text/html';
+        return Bun.file('../dist/index.html');
+      });
+  }
+
+  app.listen(3001);
 
 console.log(
   `🦊 Elysia.js is running at http://${app.server?.hostname}:${app.server?.port}`
