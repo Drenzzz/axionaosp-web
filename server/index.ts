768ee7c10cd8306b8a881713516f3810517a99e8
Drenzzz <realdrenzzz@gmail.com>
2025-10-05 09:50:23 +0700

    feat(api): create /api/devices endpoint to serve device data

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,38 @@
 import { Elysia } from 'elysia';
 import { staticPlugin } from '@elysiajs/static';
+import { cors } from '@elysiajs/cors';
+
+import devicesData from './db/devices.json';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const app = new Elysia();
+const app = new Elysia()
+  .use(cors())
+  
+  .group('/api', (app) =>
+    app.get('/devices', () => {
+      return devicesData;
+    })
+  );
 
-  if (isProduction) {
-    app
-      .use(
-        staticPlugin({
-          assets: '../dist',
-          prefix: '',
-        })
-      )
-      .get('/*', ({ set }) => {
-        set.headers['Content-Type'] = 'text/html';
-        return Bun.file('../dist/index.html');
-      });
-  }
+if (isProduction) {
+  app
+    .use(
+      staticPlugin({
+        assets: '../dist',
+        prefix: '',
+      })
+    )
+    .get('/*', ({ set }) => {
+      set.headers['Content-Type'] = 'text/html';
+      return Bun.file('../dist/index.html');
+    });
+}
 
-  app.listen(3001);
+app.listen(3001);
 
 console.log(
-  `🦊 Elysia.js is running at http://${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia server is running at http://${app.server?.hostname}:${
+    app.server?.port
+  }`
 );
